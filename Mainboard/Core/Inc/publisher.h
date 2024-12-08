f37dd4ff2f7f8a8d82a8cfad8f4e58e4898fe794
Nathaniel Correa <correanathanh@gmail.com>
2024-12-07 23:58:18 -0500

    started work on linked list, subscriber

---
+++ Mainboard/Core/Inc/publisher.h
@@ -0,0 +1,16 @@
+/*
+ * publisher.h
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: Nathaniel Correa
+ */
+
+#ifndef INC_PUBLISHER_H_
+#define INC_PUBLISHER_H_
+
+#include <stdint.h>
+#include "linked_list.h"
+
+void publish(node_t* node);
+
+#endif /* INC_PUBLISHER_H_ */
---
+++ Mainboard/Core/Inc/subscriber.h
@@ -0,0 +1,18 @@
+/*
+ * subscriber.h
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: Nathaniel Correa
+ */
+
+#ifndef INC_SUBSCRIBER_H_
+#define INC_SUBSCRIBER_H_
+
+#include <stdint.h>
+#include "linked_list.h"
+
+void subscribe(node_t* node);
+
+void update(uint32_t param_id, void* val);
+
+#endif /* INC_SUBSCRIBER_H_ */
