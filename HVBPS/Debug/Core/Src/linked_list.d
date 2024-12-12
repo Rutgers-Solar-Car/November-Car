17d7dede4c0fe59f9d0c89a50010ac729e3b5401
Nathaniel Correa <correanathanh@gmail.com>
2024-12-12 18:58:33 -0500

    checkout to change branch

--- HVBPS/Debug/Core/Src/linked_list.d
+++
@@ -1,3 +0,0 @@
-Core/Src/linked_list.o: ../Core/Src/linked_list.c \
- ../Core/Inc/linked_list.h
-../Core/Inc/linked_list.h:
--- Mainboard/Debug/Core/Src/subscribe.d
+++
@@ -1,5 +0,0 @@
-Core/Src/subscribe.o: ../Core/Src/subscribe.c ../Core/Inc/linked_list.h \
- ../Core/Inc/subscriber.h ../Core/Inc/linked_list.h
-../Core/Inc/linked_list.h:
-../Core/Inc/subscriber.h:
-../Core/Inc/linked_list.h:
